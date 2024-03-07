c033a19560b5f72d4fd0e96c603718c8bd66d96b
TAMAGO55123 <tamago.55123@gmail.com>
2024-03-07 21:05:47 +0900

    update

--- data1.py
+++ data1.py
@@ -1,41 +1,93 @@
-import l293d
-import time as sleep
+import RPi.GPIO as GPIO
+from time import sleep
 
-l293d.cleanup()
-l293d.Config.pin_numbering = 'BOARD'
+GPIO.setmode(GPIO.BOARD)
 
-motor2data = l293d.DC(15,11,13)
-motor1data=l293d.DC(22,18,16)
+motor1_1=18
+motor1_2=20
+enable1=24
+
+motor2_1=11
+motor2_2=13
+enable2=15
+
+button=40
+
+GPIO.setup(motor1_2,GPIO.OUT)
+GPIO.setup(motor1_1,GPIO.OUT)
+GPIO.setup(motor2_1,GPIO.OUT)
+GPIO.setup(motor2_2,GPIO.OUT)
+GPIO.setup(enable1,GPIO.OUT)
+GPIO.setup(enable2,GPIO.OUT)
+
+GPIO.setup(button,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+p1=GPIO.PWM(enable1,1000)
+p2=GPIO.PWM(enable2,1000)
+
+p1.start(0)
+p2.start(0)
+
+def motor(motor,forward,speed):
+    if motor == 1:
+        if forward == 1:
+            GPIO.output(motor1_1,True)
+            GPIO.output(motor1_2,False)
+        elif forward == 0:
+            GPIO.output(motor1_1,False)
+            GPIO.output(motor1_2,True)
+        p1.ChangeDutyCycle(speed)
+    elif motor == 2:
+        if forward == 1:
+            GPIO.output(motor2_1,True)
+            GPIO.output(motor2_2,False)
+        elif forward == 0:
+            GPIO.output(motor2_1,False)
+            GPIO.output(motor2_2,True)
+        p2.ChangeDutyCycle(speed)
+
+def motorstop(motor):
+    if motor == 1:
+        GPIO.output(motor1_1,False)
+        GPIO.output(motor1_2,False)
+        p1.ChangeDutyCycle(0)
+    elif motor == 2:
+        GPIO.output(motor2_1,False)
+        GPIO.output(motor2_2,False)
+        p2.ChangeDutyCycle(0)
 
 while True:
-    l293d.cleanup()
-    motor1data=l293d.DC(22,18,16)
-    motor1data.clockwise(speed=50,duration=3)
-    sleep(0.5)
-    l293d.cleanup()
-    motor1data=l293d.DC(18,22,16)
-    motor1data.clockwise(speed=50,duration=3)
-    sleep(0.5)
-    l293d.cleanup()
-    motor2data=l293d.DC(15,11,13)
-    motor2data.clockwise(speed=40,duration=3)
-    sleep(0.5)
-    l293d.cleanup()
-    motor2data=l293d.DC(11,15,13)
-    motor2data.clockwise(speed=40,duration=3)
+    if GPIO.input(button) == GPIO.HIGH:
+        motor(1,1,50)
+        sleep(3)
+        motorstop(1)
+        sleep(0.5)
 
-    l293d.cleanup()
-    motor1data=l293d.DC(22,18,16)
-    motor2data=l293d.DC(15,11,13)
-    motor1data.clockwise(speed=50,wait=False)
-    motor2data.clockwise(speed=40,duration=3)
-    motor1data.stop()
-    sleep(1)
+        motor(1,0,50)
+        sleep(3)
+        motorstop(1)
+        sleep(1)
 
-    l293d.cleanup()
-    motor1data=l293d.DC(18,22,16)
-    motor2data=l293d.DC(11,15,13)
-    motor1data.clockwise(speed=50,wait=False)
-    motor2data.clockwise(speed=40,duration=3)
-    motor1data.stop()
-    sleep(3)
+        motor(2,1,40)
+        sleep(3)
+        motorstop(2)
+        sleep(0.5)
+
+        motor(2,0,40)
+        sleep(3)
+        motorstop(2)
+        sleep(2)
+
+        motor(1,1,50)
+        motor(2,1,40)
+        sleep(3)
+        motorstop(1)
+        motorstop(2)
+        sleep(0.5)
+        
+        motor(1,0,50)
+        motor(2,0,40)
+        sleep(3)
+        motorstop(1)
+        motorstop(2)
+        sleep(1)
