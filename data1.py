fcba07b042c09fabae86b0e8265b1b6c83b0888e
TAMAGO55123 <tamago.55123@gmail.com>
2024-03-07 19:20:30 +0900

    update

--- data1.py
+++ data1.py
@@ -1,21 +1,36 @@
-import RPi.GPIO as GPIO
 import l293d
-
-GPIO.setmode(GPIO.BOARD)
+import time as sleep
 
 motor1 = l293d.DC(22,18,16)
-
 motor2 = l293d.DC(15,11,13)
 motor2data = l293d.DC(15,11,13)
-GPIO.setup(40,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 motor1data=l293d.DC(22,18,16)
+
 while True:
     motor1data=l293d.DC(22,18,16)
     motor1data.clockwise(speed=50,duration=3)
+    sleep(0.5)
     motor1data=l293d.DC(18,22,16)
     motor1data.clockwise(speed=50,duration=3)
+    sleep(0.5)
     motor2data=l293d.DC(15,11,13)
     motor2data.clockwise(speed=40,duration=3)
+    sleep(0.5)
     motor2data=l293d.DC(11,15,13)
     motor2data.clockwise(speed=40,duration=3)
-    motor1data=l293d.DC
+
+    motor1data=l293d.DC(22,18,16)
+    motor2data=l293d.DC(15,11,13)
+    motor1data.clockwise(speed=50,wait=False)
+    motor2data.clockwise(speed=40,duration=3)
+    motor1data.stop()
+    sleep(1)
+
+    motor1data=l293d.DC(18,22,16)
+    motor2data=l293d.DC(11,15,13)
+    motor1data.clockwise(speed=50,wait=False)
+    motor2data.clockwise(speed=40,duration=3)
+    motor1data.stop()
+    sleep(3)
+
+
