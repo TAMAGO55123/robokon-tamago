443d08cdce75df93dc718922d403f1e61ba51a37
TAMAGO55123 <tamago.55123@gmail.com>
2024-03-07 16:04:59 +0900

    update

--- data1.py
+++ data1.py
@@ -1,8 +1,13 @@
+import RPi.GPIO as GPIO
 import l293d
 
+GPIO.setmode(GPIO.BOARD)
+
 motor1 = l293d.DC(22,18,16)
+
 motor2 = l293d.DC(15,11,13)
+GPIO.setup(40,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 while True:
-    motor1.clockwise(speed=50,duration=5)
-    
+    if GPIO.input(40)==1:
+        motor1.clockwise(speed=50,duration=3)
